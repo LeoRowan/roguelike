6b68e2c39bfc3cc527612b6928928f345b1615fe
Jak Rowan <jak@rowan.li>
2019-11-13 14:12:11 -0500

    Add player movement

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,45 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(colors::WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root
+            .put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
+
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::{Key, KeyCode::*};
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        // Handle Movement
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        // Handle Fulscreen
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true,
+        _ => (),
+    }
+    false
 }

e0fba70112de50c61dc0021090b20e9902dae638
Jak Rowan <jak@rowan.li>
2019-11-13 23:12:23 -0500

    Generalize entities and add NPC

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,36 @@ const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
 struct Tcod {
+    con: Offscreen,
     root: Root,
+}
+
+/// This is a generic entity: the player, a monster, an item, the stairs...
+/// It's always represented by a character on screen
+struct Entity {
+    x: i32,
+    y: i32,
+    char: char,
+    color: colors::Color,
+}
+
+impl Entity {
+    fn new((x, y): (i32, i32), char: char, color: colors::Color) -> Self {
+        Entity { x, y, char, color }
+    }
+
+    /// Move the entity by the given amount
+    fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    /// set the color and draw the character that represents this entity at its
+    /// given position
+    fn draw<T: Console>(&self, con: &mut T) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
 }
 
 fn main() {
@@ -17,38 +46,57 @@ fn main() {
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Roguelike/Libtcod Tutorial")
         .init();
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    let mut tcod = Tcod { root };
+    let mut tcod = Tcod { con, root };
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    let player = Entity::new((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), '@', colors::WHITE);
+    let npc = Entity::new(
+        (SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2),
+        '@',
+        colors::YELLOW,
+    );
+
+    let mut entities = [player, npc];
 
     while !tcod.root.window_closed() {
-        tcod.root.set_default_foreground(colors::WHITE);
-        tcod.root.clear();
-        tcod.root
-            .put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.con.clear();
+
+        for entity in &entities {
+            entity.draw(&mut tcod.con);
+        }
+
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
         tcod.root.flush();
 
-        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        let player = &mut entities[0];
+        let exit = handle_keys(&mut tcod, player);
         if exit {
             break;
         }
     }
 }
 
-fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+fn handle_keys(tcod: &mut Tcod, player: &mut Entity) -> bool {
     use tcod::input::{Key, KeyCode::*};
 
     let key = tcod.root.wait_for_keypress(true);
     match key {
         // Handle Movement
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         // Handle Fulscreen
         Key {
